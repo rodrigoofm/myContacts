a7524c21f9448ad2ccfdf7597e494f478eaaece2
Rodrigo Fernandes <f.rodrigo095@gmail.com>
2022-01-17 17:51:08 -0300

    Criando o modulo de categoria

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const ContactsController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
@@ -9,5 +10,11 @@ router.get('/contacts', ContactsController.index);
 router.get('/contacts/:id', ContactsController.show);
 router.delete('/contacts/:id', ContactsController.delete);
 router.put('/contacts/:id', ContactsController.update);
+
+router.post('/categories', CategoryController.store);
+router.get('/categories', CategoryController.index);
+router.get('/categories/:id', CategoryController.show);
+router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
 
 module.exports = router;
